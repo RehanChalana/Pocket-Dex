45a2dd4e7fdc893525f3f19f27729d058362a2f7
RehanChalana <rehanchalanaprsnl@gmail.com>
2024-03-03 20:56:22 +0530

    started backend

---
+++ server/pocket-dex/src/test/java/com/rehan/pocketdex/PocketDexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rehan.pocketdex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketDexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
