d35000b51a1a48dd50a8095bd2cfc364e3d879ee
RehanChalana <rehanchalanaprsnl@gmail.com>
2024-03-11 16:14:42 +0530

    designed doughnut chart

--- server/pocket-dex/src/main/resources/static/src/js/doughnut-chart.js
+++ server/pocket-dex/src/main/resources/static/src/js/doughnut-chart.js
@@ -1,21 +1,40 @@
 const doughnutChart = document.querySelector("#bottom-doughnut-chart");
-var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-var yValues = [55, 49, 44, 24, 15];
-var barColors = [
-  "#b91d47",
-  "#00aba9",
-  "#2b5797",
-  "#e8c3b9",
-  "#1e7145"
-];
+
+
+fetchData().then(data => {
+  console.log(data);
+  let categories = {};
+
+    // Loop through transactions to calculate total amount spent on each category
+    data.forEach(transaction => {
+        const category = transaction.transaction_category;
+        const amount = parseFloat(transaction.transaction_amount);
+
+        // If category doesn't exist in categories object, create it with initial amount
+        if (!categories[category]) {
+            categories[category] = amount;
+        } else {
+            // If category already exists, add amount to its total
+            categories[category] += amount;
+        }
+    });
+    // Extract category names and corresponding amounts into separate arrays
+    const categoryNames = Object.keys(categories);
+    const categoryAmounts = categoryNames.map(category => categories[category]);
+    console.log(categoryNames);
+    console.log(categoryAmounts);
+
 
 new Chart(doughnutChart, {
   type: "doughnut",
   data: {
-    labels: xValues,
+    labels: categoryNames,
     datasets: [{
-      backgroundColor: barColors,
-      data: yValues
+      data: categoryAmounts,
+      borderWidth:3,
+      borderColor: ['rgba(232,241,103,1)','rgba(103,241,181,1)','rgba(103,241,112,1)','rgba(241,99,163,1)','rgba(139,241,103,1)','rgba(29,175,166,1)'],
+      backgroundColor : ['rgba(232,241,103,0.2)','rgba(103,241,181,0.2)','rgba(103,241,112,0.2)','rgba(241,99,163,0.2)','rgba(139,241,103,0.2)','rgba(29,175,166,0.2)'],
+      
     }]
   },
   options: {
@@ -23,17 +42,21 @@ new Chart(doughnutChart, {
       legend: {
         display: true,
         position: 'bottom',
-        align: 'center',
-        maxItems: 1, // Display only one item per row
         labels: {
           boxWidth: 20,
           padding: 20
         }
       }
     },
+    cutout:65,
     title: {
       display: true,
-      text: "World Wide Wine Production 2018"
     }
   }
 });
+})
+
+
+['rgba(232,241,103,0.2)','rgba(103,241,181,0.2)','rgba(103,241,112,0.2)','rgba(241,99,163,0.2)','rgba(139,241,103,0.2)','rgba(29,175,166,0.2)']
+
+
