b0fcffde13aa32f92b9edf2e979c8804d872931d
RehanChalana <rehanchalanaprsnl@gmail.com>
2024-03-15 19:46:31 +0530

    history sorting working now

--- server/pocket-dex/src/main/resources/static/src/js/history.js
+++ server/pocket-dex/src/main/resources/static/src/js/history.js
@@ -47,7 +47,7 @@ fetchData().then(data => {
   transaction_data = data;
   for(let key in transaction_data) {
    let listEntry = document.createElement("div");
-   listEntry.classList.add("history-list-entry","flex","justify-between","px-4","py-3","text-gray-300","text-medium","font-medium","rounded-xl","items-center","hover:font-semibold","hover:text-xl","hover:px-5","hover:py-4","ease-in-out","duration-300","hover:bg-neutral-800");
+   listEntry.classList.add("history-list-entry","flex","justify-between","px-4","py-3","text-gray-300","text-medium","font-medium","rounded-xl","items-center","hover:font-semibold","hover:text-xl","ease-in-out","duration-300","hover:text-gray-800");
    listEntry.innerHTML = `<div class=history-entry-options-icon"><i class="fa-solid fa-ellipsis-vertical fa-lg"></i></div>
                           <div class="history-entry-date">${transaction_data[key].transaction_date}</div>
                           <div class="history-entry-name">${transaction_data[key].transaction_title}</div>
@@ -58,6 +58,47 @@ fetchData().then(data => {
 }
   
 });
+
+const sortSearchButton = document.querySelector(".sort-search-button");
+
+sortSearchButton.addEventListener("click" , function () {
+  let fromDate = document.querySelector(".header-from-date").value;
+  let toDate = document.querySelector(".header-to-date").value;
+
+const date1 = new Date(fromDate);
+const date2 = new Date(toDate);
+
+  fetchData().then(data => {
+
+    const filteredTransactions = data.filter(transaction => {
+      const transactionDate = new Date(transaction.transaction_date.replace(/-/g, '/'));
+      return transactionDate >= date1 && transactionDate <= date2;
+  });
+
+  console.log(filteredTransactions);
+  
+  // Sort filtered transactions by transaction date
+  filteredTransactions.sort((a, b) => {
+      const dateA = new Date(a.transaction_date.replace(/-/g, '/'));
+      const dateB = new Date(b.transaction_date.replace(/-/g, '/'));
+      return dateA - dateB;
+  });
+
+  historyListContainer.innerHTML = "";
+
+  for(let key in filteredTransactions) {
+    let listEntry = document.createElement("div");
+    listEntry.classList.add("history-list-entry","flex","justify-between","px-4","py-3","text-gray-300","text-medium","font-medium","rounded-xl","items-center","hover:font-semibold","hover:text-xl","ease-in-out","duration-300","hover:text-gray-800");
+    listEntry.innerHTML = `<div class=history-entry-options-icon"><i class="fa-solid fa-ellipsis-vertical fa-lg"></i></div>
+                           <div class="history-entry-date">${filteredTransactions[key].transaction_date}</div>
+                           <div class="history-entry-name">${filteredTransactions[key].transaction_title}</div>
+                           <div class="history-entry-wallet">${filteredTransactions[key].wallet_name}</div>
+                           <div class="history-entry-amount">${filteredTransactions[key].transaction_amount}</div>
+                           <div class="history-entry-check text-gray-800 px-2 py-1 font-semibold rounded-lg">Completed</div>`;
+    historyListContainer.appendChild(listEntry);
+ }
+  })
+})
 
 
 
